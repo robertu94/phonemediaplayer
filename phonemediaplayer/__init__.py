aa074ad47e9488943d832c1142066d792ac1e19b
Robert Underwood <rr.underwood94@gmail.com>
2022-02-25 10:02:38 -0500

    several fixes to status

--- phonemediaplayer/__init__.py
+++ phonemediaplayer/__init__.py
@@ -4,12 +4,16 @@ import argparse
 import dbus
 import re
 import time
+from collections import OrderedDict
 
 sys_bus = dbus.SystemBus()
 
+
 def connect_a_phone():
     adapter_object = sys_bus.get_object("org.bluez", "/org/bluez/hci0")
-    adapter_properties = dbus.Interface(adapter_object, "org.freedesktop.DBus.Properties")
+    adapter_properties = dbus.Interface(
+        adapter_object, "org.freedesktop.DBus.Properties"
+    )
     powered = adapter_properties.Get("org.bluez.Adapter1", "Powered")
     if not powered:
         adapter_properties.Set("org.bluez.Adapter1", "Powered", dbus.Boolean(True))
@@ -19,25 +23,37 @@ def connect_a_phone():
     phones = [
         path
         for path, interfaces in objects.GetManagedObjects().items()
-        if "org.bluez.Device1" in interfaces and
-           "phone" in interfaces["org.bluez.Device1"]["Icon"].lower()
+        if "org.bluez.Device1" in interfaces
+        and "phone" in interfaces["org.bluez.Device1"]["Icon"].lower()
     ]
-
 
     device_object = sys_bus.get_object("org.bluez", phones[0])
     device_interface = dbus.Interface(device_object, "org.bluez.Device1")
     device_properties = dbus.Interface(device_object, "org.freedesktop.DBus.Properties")
-    print("connecting", device_properties.Get("org.bluez.Device1","Name"))
+    print("connecting", device_properties.Get("org.bluez.Device1", "Name"))
     device_interface.Connect()
-    #the connect call doesn't block, so wait for it
+    # the connect call doesn't block, so wait for it
     time.sleep(4)
-
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("action", choices=["play", "pause", "stop", "next", "previous", "playpause", "status"], help="action to do")
+    parser.add_argument(
+        "action",
+        help="""action to do, fuzzy matches (default toggle)
+"play" play the track
+"pause" pause the track
+"stop" stop the track
+"snext" skip ahead
+"sprevious" skip back
+"next" next track
+"previous" previous track
+"toggle" play/pause
+"status" show playing track
+    """,
+    )
     return parser.parse_args()
+
 
 def get_players():
     """returns a list of interfaces that provide the player interface"""
@@ -46,10 +62,11 @@ def get_players():
     objects = dbus.Interface(bluez_objects, "org.freedesktop.DBus.ObjectManager")
 
     return [
-        object for object 
-        in objects.GetManagedObjects().keys() 
-        if  player_re.match(object)
+        object
+        for object in objects.GetManagedObjects().keys()
+        if player_re.match(object)
     ]
+
 
 def play_pause(device, player_status):
     if player_status == "playing":
@@ -57,43 +74,120 @@ def play_pause(device, player_status):
     elif player_status == "paused":
         device.Play()
 
+
 def status(name, status):
     if status == "playing":
         print(status, name)
     elif status == "paused":
         print(status)
 
+
+def fuzzy_match(entries, keyword, default):
+    def score(a, b):
+        s = 0
+        for ai, bi in zip(a, b):
+            if ai == bi:
+                s += 1
+            else:
+                break
+        return s
+
+    best = default
+    best_score = 0
+    for entry in entries:
+        if entry == keyword:
+            return keyword
+        s = score(entry, keyword)
+        if s > best_score:
+            best = entry
+            best_score = s
+    return best
+
+
 def manage_player(args):
     player_path = get_players()[0]
     player_object = sys_bus.get_object("org.bluez", player_path)
     player_interface = dbus.Interface(player_object, "org.bluez.MediaPlayer1")
     player_properties = dbus.Interface(player_object, "org.freedesktop.DBus.Properties")
-    player_status = player_properties.Get("org.bluez.MediaPlayer1","Status")
-    track = player_properties.Get("org.bluez.MediaPlayer1", "Track")
-    title = track.get('Title', "")
-    album = track.get('Album', "")
-    artist = track.get('Artist', "")
+    player_status = player_properties.Get("org.bluez.MediaPlayer1", "Status")
     try:
-        name = player_properties.Get("org.bluez.MediaPlayer1","Name")
+        track = player_properties.Get("org.bluez.MediaPlayer1", "Track")
+    except dbus.exceptions.DBusException:
+        track = {
+            "Title": "",
+            "Album": "",
+            "Artist": "",
+            "Duration": -1,
+            "TrackNumber": -1,
+            "NumberOfTracks": -1,
+        }
+    title = track.get("Title", "").strip()
+    album = track.get("Album", "").strip()
+    artist = track.get("Artist", "").strip()
+    current_track = track.get("TrackNumber", -1)
+    tracks = track.get("NumberOfTracks", -1)
+    artist = track.get("Artist", "").strip()
+    duration = int(track.get("Duration", -1))
+    position = player_properties.Get("org.bluez.MediaPlayer1", "Position")
+    try:
+        name = player_properties.Get("org.bluez.MediaPlayer1", "Name")
     except dbus.DBusException:
         name = ""
 
-    if name == "Overcast":
-        full_status = f"{name}: {artist} {title} {album}"
+    min_progress = position // 60000
+    sec_progress = (position - min_progress * 60000) // 1000
+    min_total = duration // 60000
+    sec_total = (duration - min_total * 60000) // 1000
+    if duration != -1:
+        duration_str = f"{min_progress}:{sec_progress:02d}/{min_total}:{sec_total:02d}"
     else:
-        full_status = f"{name}: {album} {title}"
+        duration_str = ""
 
-    ACTIONS = {
-     "playpause": lambda : play_pause(player_interface, full_status),
-     "play": lambda : player_interface.Play(),
-     "pause": lambda : player_interface.Pause(),
-     "stop": lambda : player_interface.Pause(),
-     "next": lambda : player_interface.Next(),
-     "previous": lambda : player_interface.Previous(),
-     "status": lambda: status(full_status, player_status)
-    }
+    if tracks > 0 and current_track > 0:
+        track_str = f"track = {current_track}/{tracks},"
+    else:
+        track_str = ""
 
-    ACTIONS[args.action]()
+    album_sep = "on" if not "Overcast" in name else "-"
+    if title:
+        if album:
+            if artist:
+                desc = f'{artist} - "{title}" {album_sep} {album}'
+            else:
+                desc = f'"{title}" {album_sep} {album}'
+        elif artist:
+            desc = f'{artist} {album_sep} {album}'
+        else:
+            desc = f'"{title}"'
+    elif album:
+        if artist:
+            desc = f"{artist} {album_sep} {album}"
+        else:
+            desc = f"{album}"
+    elif artist:
+        desc = "{artist}"
+    else:
+        desc = ""
+
+
+    full_status = f'{name}: {desc} {track_str} {duration_str}'.strip()
+
+    ACTIONS = OrderedDict(
+        [
+            ("toggle", lambda: play_pause(player_interface, player_status)),
+            ("play", lambda: player_interface.Play()),
+            ("status", lambda: status(full_status, player_status)),
+            ("pause", lambda: player_interface.Pause()),
+            ("stop", lambda: player_interface.Pause()),
+            ("next", lambda: player_interface.FastForward()),
+            ("previous", lambda: player_interface.Rewind()),
+            ("snext", lambda: player_interface.Next()),
+            ("sprevious", lambda: player_interface.Previous()),
+        ]
+    )
+
+    ACTIONS[fuzzy_match(ACTIONS.keys(), args.action, default="toggle")]()
+
 
 def main():
     args = parse_args()
@@ -105,7 +199,6 @@ def main():
             manage_player(args)
         except IndexError:
             print("could not connect a phone")
-
 
 
 if __name__ == "__main__":
